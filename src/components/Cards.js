701ed47d37e38d96d11dd50dddb33cfa5c5585d0
cookieman2002 <mcl2002@outlook.dk>
2022-09-05 09:08:50 +0200

    image update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,7 +6,7 @@ const Cards = () => {
       brand: "HP",
       price: 10000,
       image:
-        "https://assets.petco.com/petco/image/upload/f_auto,q_auto/green-tree-frog-care-sheet-hero",
+      "https://www.elgiganten.dk/resource/responsive-image/200222/image_text/mobile/4/315/189/lenovo-legion-with-an-amd-processor.jpg",
     },
     {
       id: 2,
@@ -14,7 +14,7 @@ const Cards = () => {
       brand: "HP",
       price: 10000,
       image:
-        "https://assets.petco.com/petco/image/upload/f_auto,q_auto/green-tree-frog-care-sheet-hero",
+      "https://www.elgiganten.dk/resource/responsive-image/200222/image_text/mobile/4/315/189/lenovo-legion-with-an-amd-processor.jpg",
     },
     {
       id: 3,
@@ -22,7 +22,7 @@ const Cards = () => {
       brand: "HP",
       price: 10000,
       image:
-        "https://assets.petco.com/petco/image/upload/f_auto,q_auto/green-tree-frog-care-sheet-hero",
+      "https://www.elgiganten.dk/resource/responsive-image/200222/image_text/mobile/4/315/189/lenovo-legion-with-an-amd-processor.jpg",
     },
     {
       id: 4,
@@ -30,7 +30,7 @@ const Cards = () => {
       brand: "HP",
       price: 10000,
       image:
-        "https://assets.petco.com/petco/image/upload/f_auto,q_auto/green-tree-frog-care-sheet-hero",
+      "https://www.elgiganten.dk/resource/responsive-image/200222/image_text/mobile/4/315/189/lenovo-legion-with-an-amd-processor.jpg",
     },
     {
       id: 5,
@@ -51,7 +51,7 @@ const Cards = () => {
   ];
 
   return (
-    <div className="xl:grid flex flex-col xl:grid-cols-3 gap-11 items-center xl:ml-16 xl:mt-5 mt-10">
+    <div className="xl:grid flex flex-col xl:grid-cols-3 gap-11 items-center p-0 xl:ml-16 xl:mt-5 mt-10">
       {data.map((info) => (
         <div className=" group bg-background" key={info.id}>
             <img className="w-64 h-64" src={info.image} alt="" />
